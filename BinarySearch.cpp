bd5c1735bfdb51daeec60fac7b5fdb6e8c7f2f07
pichiefox <citrazad@gmail.com>
2026-04-16 17:18:41 +0700

    cout "Masukkan Elemen Array "

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -21,4 +21,9 @@ void input()
             cout << "\n[!] Jumlah elemen tidak boleh lebih dari 10. Silahkan coba lagi.\n";
         }
     }
+
+    cout << "\n=====================\n";
+    cout << "  Masukkan Elemen Array \n";
+    cout << "\n=====================\n";
+
 }
