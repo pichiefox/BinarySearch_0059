d725b148f179a1b0cfa067d10a1ef1d83f688ba0
pichiefox <citrazad@gmail.com>
2026-04-16 17:13:49 +0700

    if

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -11,5 +11,10 @@ void input()
     {
         cout << "Masukkan banyaknya elemen pada array (maksimal 10): "
         cin >> nPanjang;
+
+        if (nPanjang <= 10)
+        {
+            cout << "\n[!] Jumlah elemen tidak boleh lebih dari 10. Silahkan coba lagi.\n";
+        }
     }
 }
